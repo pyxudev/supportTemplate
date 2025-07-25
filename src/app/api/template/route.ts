cee85f3a3557781da1b031cf7566b436e9df0cc1
Justin Xu <92786015+pyxudev@users.noreply.github.com>
2025-07-26 00:57:15 +0900

    Update route.ts using secrete

--- src/app/api/template/route.ts
+++ src/app/api/template/route.ts
@@ -1,5 +1,5 @@
 export async function POST() {
-    const dbUrl = "https://pgw4s4ucnbaqq53432jeno4v440ufjpk.lambda-url.ap-northeast-1.on.aws/";
+    const dbUrl = process.env.TEMPLATE_LIST_AWS_LAMBDA;
     const response = await fetch(dbUrl);
     const data = await response.json();
     return new Response(JSON.stringify(data), {
